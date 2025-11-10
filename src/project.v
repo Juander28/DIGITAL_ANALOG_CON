64723d435c9b0cf87c2353133eac782ef91e5ebc
Juander28 <85529742+Juander28@users.noreply.github.com>
2025-11-10 06:45:38 -0800

    Add files via upload

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module DIGITAL_ANALOG_CON (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
